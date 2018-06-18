04f543752999441140542d98987b14cd9a98d356
Mhamidi <hamidi.michael@outlook.fr>
2018-06-18 16:49:52 +0200

    18-06-18

---
+++ HelloDev/Controllers/RestaurantController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloDev.Models;
+
+namespace HelloDev.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create(Restaurant _resto)
+        {
+            return View(); 
+        }
+
+        public ActionResult Edit(Restaurant _resto)
+        {
+            return View();
+        }
+    }
+}
